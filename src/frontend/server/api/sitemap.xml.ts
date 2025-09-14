f5349342619f21e1dcdd7c1d987087b14615940d
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2025-09-14 17:25:38 +0000

    cleanup + sitemap update

--- src/frontend/server/api/sitemap.xml.ts
+++ src/frontend/server/api/sitemap.xml.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async event => {
     // Set XML content type
     setHeader(event, 'Content-Type', 'application/xml')
 
-    const baseUrl = 'https://nftropoly.com'
+    const baseUrl = 'https://ionicswap.com'
     const currentDate = new Date().toISOString()
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
@@ -16,28 +16,34 @@ export default defineEventHandler(async event => {
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>${baseUrl}/nfts</loc>
+    <loc>${baseUrl}/markets</loc>
     <lastmod>${currentDate}</lastmod>
     <changefreq>hourly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>${baseUrl}/collections</loc>
+    <loc>${baseUrl}/trading</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>hourly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/wallet</loc>
     <lastmod>${currentDate}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>${baseUrl}/tokens</loc>
+    <loc>${baseUrl}/support</loc>
     <lastmod>${currentDate}</lastmod>
-    <changefreq>hourly</changefreq>
-    <priority>0.8</priority>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
   </url>
   <url>
-    <loc>${baseUrl}/activity</loc>
+    <loc>${baseUrl}/contact</loc>
     <lastmod>${currentDate}</lastmod>
-    <changefreq>hourly</changefreq>
-    <priority>0.7</priority>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
   </url>
   <!-- Profile URLs will be added dynamically when we implement the getAllUsernames endpoint -->
 </urlset>`
@@ -47,7 +53,7 @@ export default defineEventHandler(async event => {
     console.error('Error generating sitemap:', error)
 
     // Return basic sitemap on error
-    const baseUrl = 'https://nftropoly.com'
+    const baseUrl = 'https://ionicswap.com'
     return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
